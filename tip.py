5348f6032282d27410c4175a9e7d6c18af5c7ba7
mcbabo <67439212+mcbabo@users.noreply.github.com>
2021-06-28 00:39:23 +0200

    Update tip.py

--- tip.py
+++ tip.py
@@ -1,2 +1,22 @@
 import discord
+from discord.ext import commands
+import csv
+import datetime
 
+bot = commands.Bot(command_prefix = "!")
+
+@bot.command()
+async def nextgame(ctx):
+    with open('games.csv', mode='r') as games_file:
+        reader = csv.reader(games_file)
+        line = str(next(reader))
+        remove_char = ["[", "]"]
+        for character in remove_char:
+            line = line.replace(character, "")
+    await ctx.send(f"Nächstes Spiel ist am {line}")
+
+@bot.command()
+async def tip(ctx, game_tip):
+    with open('tip.csv', mode='w') as tip_file:
+        tip_writer = csv.writer(tip_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        tip_writer.writerow([ctx.author, game_tip])
